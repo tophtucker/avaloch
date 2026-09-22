b00b2d0c8570b79018dfa7d5565055006ac32e3b
Toph Tucker <tophtucker@gmail.com>
2026-09-22 10:26:21 -0400

    add carto api key

--- src/lib/places/InteractivePlaceMap.svelte
+++ src/lib/places/InteractivePlaceMap.svelte
@@ -24,7 +24,7 @@
 			attributionControl: false
 		});
 
-		L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+		L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png?key=cb1_3trs_1_ebebfde2a859cbc82f20035d', {
 			attribution:
 				'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
 			maxZoom: 19
